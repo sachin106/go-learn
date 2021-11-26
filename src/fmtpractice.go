3b563bc3d42d50e67c345286bf3627c27e934940
Sachin Mokashe <sachin.mokashe@veritas.com>
2021-11-26 01:37:05 -0800

    My new program of a short declaration...

---
+++ src/fmtpractice.go
@@ -0,0 +1,10 @@
+package main
+import ("fmt")
+
+func main(){
+name:= "sachin"
+lastN := "mokashe"
+age := 30
+
+fmt.Printf("%s %s age is %d years old.", name, lastN, age);
+}
---
+++ src/shortdeclaration.go
@@ -0,0 +1,11 @@
+package main
+import ("fmt")
+
+func main()	{
+
+	var1 := 10;
+	var2 := 30;
+	var3 := var1 + var2;
+	var2 = 50
+	fmt.Printf("Addition of %d + %d = %d", var1, var2, var3);
+}
